62fa3a50140a448f507948829afc1a28dfc996cb
ibrokemypie <ibrokemypie@bastardi.net>
2017-04-07 20:57:49 +1000

    minified .min and svg

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,16 +3,31 @@ module.exports = function(grunt) {
   grunt.initConfig({
 	uglify: {
     	my_target: {
-        	files: { 'dist/vendor.js': require('wiredep')().js
+        	files: { 'dist/vendor.min.js': require('wiredep')().js
 		}   }   },
 	cssmin: {
 		 minify: {
-        	files: { 'dist/vendor.css': ['src/*.css']
+		 	options: {
+		 		'inline': ['all'],
+		 		'processImport': true,
+		 		'rebase': true
+		 	},
+        	files: { 'dist/vendor.min.css': ['**/*.css', '!**/*.min.css']
 		}   }   },
+	svgmin: {
+			dist: {
+				files: [{
+					src: 'src/header.svg',
+					dest: 'dist/header.min.svg'
+				}]
+
+		}
+	},
   });
 
+grunt.loadNpmTasks('grunt-svgmin');
 grunt.loadNpmTasks('grunt-contrib-uglify');
 grunt.loadNpmTasks('grunt-contrib-cssmin');
 grunt.loadNpmTasks('grunt-wiredep');
-grunt.registerTask('default', ['cssmin', 'uglify']);
+grunt.registerTask('default', ['cssmin', 'uglify', 'svgmin']);
 };
